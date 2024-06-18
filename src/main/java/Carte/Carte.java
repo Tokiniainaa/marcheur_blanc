bb66fbcbd547a1a48b8f4cf808f215566f18723a
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-18 11:23:31 +0300

    chore: add dependancy

---
+++ src/main/java/Carte/Carte.java
@@ -0,0 +1,19 @@
+package com.marcheurblanc;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Carte {
+
+
+}
+
